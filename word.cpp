5085e27d298ff3da0e7f36903fb67f2df74eb00f
Juneyoung Lee <aqjune@gmail.com>
2020-03-19 15:23:44 +0900

    Use std::map instead of std::vector

--- word.cpp
+++ word.cpp
@@ -1,37 +1,28 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
 int main(int argv, char **argc) {
-  vector<pair<string, int>> words;
+  map<string, int> words;
   ifstream fin(argc[1]);
   string nextword;
 
   while (!fin.eof()) {
     fin >> nextword;
-
-    bool found = false;
-    for (auto &[word, occurrence] : words) {
-      if (word == nextword) {
-        ++occurrence;
-        found = true;
-        break;
-      }
-    }
-    if (!found)
-      words.emplace_back(nextword, 1);
+    words[nextword]++;
   }
 
-  string word_with_max_occur = words[0].first;
-  int max_occur = words[0].second;
-  for (auto &[word, occur] : words)
-    if (occur > max_occur) {
-      word_with_max_occur = word;
-      max_occur = occur;
+  auto answer = max_element(words.begin(), words.end(),
+    // Define a comparator
+    [](auto &elem1, auto &elem2) {
+      // Compare the occurrence first.
+      return tie(elem1.second, elem1.first) < tie(elem2.second, elem2.first);
     }
+  );
 
-  cout << "Word '" << word_with_max_occur << "' appeared " << max_occur
+  cout << "Word '" << answer->first << "' appeared " << answer->second
        << " times." << endl;
   return 0;
 }
